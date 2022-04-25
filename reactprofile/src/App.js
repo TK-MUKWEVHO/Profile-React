fbd2172993ef1437534bb6fbab579f1fee60c925
TK-MUKWEVHO <matodzikutama@gmail.com>
2022-04-25 19:48:10 +0200

    new

--- reactprofile/src/App.js
+++ reactprofile/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css';
+import AboutMe from './components/AboutMe/AboutMe';
 import Footer from './components/Footer/Footer';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <>
-      <NavBar/>
+       <NavBar/>
+      <AboutMe/>
       <Footer/>
     </> 
   )
---
+++ reactprofile/src/components/AboutMe/AboutMe.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./styles.css"
+
+const AboutMe = () => {
+  return (
+    <div className='aboutDiv'>
+        <div className="name">
+            <h1>Takalani Maxwell Mukwevho Profile</h1>
+            <p>I am a self-motivated graduate of IT: Software Development at Tshwane University of Technology. I am aiming
+            to use the knowledge that I have acquired from my coursework as well as self-study to fulfill my role in your organisation . I am also open to learning skills and Technologies. I am able to work independently in a busy
+            environment and also within a team setting.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default AboutMe
